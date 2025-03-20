462a7c6f53520182900f59fab9827ff85b4c3d70
n[oO]ne <lominum@protonmail.com>
2025-03-20 10:29:03 +0700

    refactor(config): add the possibility to pass an existing config to the service provider.

---
+++ tests/Unit/ServiceProviderTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit;
+
+use Codeception\Test\Unit;
+use Phalcon\Config\Adapter\Ini;
+use Phalcon\Config\Config;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Sentry\ServiceProvider;
+use Throwable;
+
+class ServiceProviderTest extends Unit
+{
+    /**
+     * @covers \Phalcon\Sentry\ServiceProvider::__construct
+     * @covers \Phalcon\Sentry\ServiceProvider::boot
+     * @covers \Phalcon\Sentry\ServiceProvider::mergeConfig
+     *
+     * @throws Throwable
+     * @return void
+     */
+    public function testConstructFromFile()
+    {
+        $di = new FactoryDefault();
+        $sp = new ServiceProvider(
+            dirname(__DIR__) . '/config/files/sentry.ini'
+        );
+        $sp->register($di);
+
+        $config = $di->getShared('phalcon-sentry.config');
+
+        // Assert we have a valid config
+        $this->assertInstanceOf(Config::class, $config);
+        // Assert that we merged all values
+        $this->assertEquals($config->path('test.value'), 'test');
+    }
+
+    /**
+     * @covers \Phalcon\Sentry\ServiceProvider::__construct
+     * @covers \Phalcon\Sentry\ServiceProvider::boot
+     * @covers \Phalcon\Sentry\ServiceProvider::mergeConfig
+     *
+     * @throws Throwable
+     * @return void
+     */
+    public function testConstructFromConfig()
+    {
+        $config = new Ini(dirname(__DIR__) . '/config/files/sentry.ini');
+        $di     = new FactoryDefault();
+        $sp     = new ServiceProvider($config);
+        $sp->register($di);
+
+        $config = $di->getShared('phalcon-sentry.config');
+
+        // Assert we have a valid config
+        $this->assertInstanceOf(Config::class, $config);
+        // Assert that we merged all values
+        $this->assertEquals($config->path('test.value'), 'test');
+    }
+}
